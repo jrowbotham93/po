c8584312c814e6bd6e4f9ffa3befa85b6e7a3cba
gijslebesque <gijslebesque@gmail.com>
2020-02-08 17:26:53 +0100

    nav fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,48 +1,53 @@
-
-import React from "react";
+import React from 'react';
 import Menu from '../components/Menu.js';
 import Divider from '../components/Divider';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 import Card from '../components/Card.js';
 function Contact() {
-    return(
+  return (
     <React.Fragment>
-        <section className="section-a">
-            <Menu/>
-            <div className="container-b text-align">
-                <Header
-                    styleA={"text-md text-black line-height"}
-                    styleB={"text-md-hover"}
-                    text={"Get in touch "}
-                />
-            <div className="flex-center flex-row text-align">
-                <div>
-                <Card
-                    head={"Eugene Blokhin"}
-                    subHead={"Logistics & Marketing"}
-                    desc={"E-mail: [iryna.sholokhova@proorganica.com](mailto:iryna.sholokhova@proorganica.com"}>
-                    >
-                </Card>
-                </div>
-                <div>
-                <Card
-                    head={"Eugene Blokhin"}
-                    subHead={"Logistics & Marketing"}
-                    desc={"E-mail: [iryna.sholokhova@proorganica.com](mailto:iryna.sholokhova@proorganica.com"}>
-                </Card>
-                </div>
-                <div class="padding-top-10">
-                <Card
-                    head={"Eugene Blokhin"}
-                    subHead={"Logistics & Marketing"}
-                    desc={"E-mail: [iryna.sholokhova@proorganica.com](mailto:iryna.sholokhova@proorganica.com"}>
-                </Card>
-              </div>
+      <section className="section-a">
+        <div className="container-b text-align">
+          <Header
+            styleA={'text-md text-black line-height'}
+            styleB={'text-md-hover'}
+            text={'Get in touch '}
+          />
+          <div className="flex-center flex-row text-align">
+            <div>
+              <Card
+                head={'Eugene Blokhin'}
+                subHead={'Logistics & Marketing'}
+                desc={
+                  'E-mail: [iryna.sholokhova@proorganica.com](mailto:iryna.sholokhova@proorganica.com'
+                }
+              >
+                >
+              </Card>
+            </div>
+            <div>
+              <Card
+                head={'Eugene Blokhin'}
+                subHead={'Logistics & Marketing'}
+                desc={
+                  'E-mail: [iryna.sholokhova@proorganica.com](mailto:iryna.sholokhova@proorganica.com'
+                }
+              ></Card>
+            </div>
+            <div class="padding-top-10">
+              <Card
+                head={'Eugene Blokhin'}
+                subHead={'Logistics & Marketing'}
+                desc={
+                  'E-mail: [iryna.sholokhova@proorganica.com](mailto:iryna.sholokhova@proorganica.com'
+                }
+              ></Card>
             </div>
           </div>
-        </section>
-      <Footer/>
+        </div>
+      </section>
+      <Footer />
     </React.Fragment>
   );
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,23 +1,23 @@
-
-import React from "react";
+import React from 'react';
 import Menu from '../components/Menu.js';
 import Grid from '../components/Grid.js';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 
 function Products() {
-    return(
+  return (
     <React.Fragment>
-        <section className="section-a">
-          <Menu/>
-            <div className="container-a">
-              <Header
-                  style={"ui huge header"}
-                  text={"Drop us an e-mail if you would like to find out more about ProOrganica and our work in Ukraine"}
-              />
-            </div>
-        </section>
-      <Footer/>
+      <section className="section-a">
+        <div className="container-a">
+          <Header
+            style={'ui huge header'}
+            text={
+              'Drop us an e-mail if you would like to find out more about ProOrganica and our work in Ukraine'
+            }
+          />
+        </div>
+      </section>
+      <Footer />
     </React.Fragment>
   );
 }
